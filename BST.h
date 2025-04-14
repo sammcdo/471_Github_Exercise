9aac1541aec1ce6bb4977d6891c0692e9e800c83
Sam <sam_mcdo@yahoo.com>
2025-04-14 14:34:54 -0400

    Implemented Post Order

--- BST.h
+++ BST.h
@@ -32,6 +32,7 @@ private:
   BSTNode<Key,E>* removehelp(BSTNode<Key, E>*, const Key&);
   E* findhelp(BSTNode<Key, E>*, const Key&) const;
   void printhelp(BSTNode<Key, E>*, int) const;
+  void printPostOrder(BSTNode<Key, E>*, int) const;
   void visit(BSTNode<Key, E>*) const;
 
 public:
@@ -89,7 +90,9 @@ public:
 
   void print() const { // Print the contents of the BST
     if (root == NULL) cout << "The BST is empty.\n";
-    else printhelp(root, 0);
+    else  {
+      printPostOrder(root, 0);
+    }
   }
   
 };
@@ -196,4 +199,14 @@ printhelp(BSTNode<Key, E>* root, int level) const {
   printhelp(root->left(), level+1);   // Do left subtree
   visit(root);						  // Print node value
   printhelp(root->right(), level+1);  // Do right subtree
+}
+
+
+template <typename Key, typename E>
+void BST<Key, E>::
+printPostOrder(BSTNode<Key, E>* root, int level) const {
+  if (root == NULL) return;                // Empty tree
+  printPostOrder(root->left(), level+1);   // Do left subtree
+  printPostOrder(root->right(), level+1);  // Do right subtree
+  visit(root);	               					   // Print node value
 }
